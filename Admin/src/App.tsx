517c541937201505da3d13741b5cc9665f276fdf
Dharmangi <129186750+Dharmangi@users.noreply.github.com>
2025-05-21 18:44:36 +0530

    Add files via upload

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -1,24 +1,38 @@
+// App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./Components/Navbar/Navbar";
-import { Footer } from "./Components/Footer/Footer";
+// import { Footer } from "./Components/Footer/Footer";
 import { AdminLogin } from "./Components/AdminLogin/AdminLogin";
-import  Dashboard  from "./Components/AdminDashboard/Dashboard"; 
-import AddUser from './Pages/User/AddUser';
+import Dashboard from "./Components/AdminDashboard/Dashboard";
+import AddUser from "./Pages/User/AddUser";
 import AddDepartment from "./Pages/Department/AddDepartment";
+import AdminRegister from "./Components/AdminRegister/AdminRegister";
+import "./App.css"; // For layout styling
+
 function App() {
   return (
     <BrowserRouter>
       <div className="app-container">
+        {/* Navbar at top */}
         <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<AdminLogin />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/add-user" element={<AddUser />} />
- <Route path="/add-department" element={<AddDepartment />} />
-          </Routes>
-        </main>
-        <Footer />
+
+        {/* Content area: Sidebar + Main */}
+        <div className="content-layout">
+          {/* <Sidebar /> */}
+
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<AdminLogin />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/adduser" element={<AddUser />} />
+              <Route path="/adddepartment" element={<AddDepartment />} />
+              <Route path="/adminregister" element={<AdminRegister/>} />
+            </Routes>
+          </main>
+        </div>
+
+        {/* Footer at bottom */}
+        {/* <Footer /> */}
       </div>
     </BrowserRouter>
   );
