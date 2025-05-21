e4236e1af7baf38712c1e9aa78cbbe67e54c937a
Dharmangi <129186750+Dharmangi@users.noreply.github.com>
2025-05-21 18:43:45 +0530

    Add files via upload

--- Admin/src/Components/Sidebar/Sidebar.tsx
+++ Admin/src/Components/Sidebar/Sidebar.tsx
@@ -1,1 +1,50 @@
+import React, { useState } from 'react';
+import { FaTachometerAlt, FaUserPlus, FaUsers, FaBuilding } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import './sidebar.css';
 
+const Sidebar: React.FC = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    return (
+        <div 
+            className={`sidebar ${isExpanded ? 'expanded' : ''}`}
+            onMouseEnter={() => setIsExpanded(true)}
+            onMouseLeave={() => setIsExpanded(false)}
+        >
+            <ul>
+                <li>
+                  <FaTachometerAlt />
+                    {isExpanded && 
+                  <Link to="/dashboard">
+                    <span>Dashboard</span>
+                  </Link>}
+                    
+                </li>
+                <li>
+                    <FaUserPlus />
+                    {isExpanded && 
+                  <Link to="/adminregister">
+                    <span>Add Admin</span>
+                  </Link>}
+                </li>
+                <li>
+                    <FaUsers />
+                    {isExpanded && 
+                  <Link to="/adduser">
+                    <span>User Creation</span>
+                  </Link>}
+                </li>
+                <li>
+                    <FaBuilding />
+                    {isExpanded && 
+                  <Link to="/adddepartment">
+                    <span>Department</span>
+                  </Link>}
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
