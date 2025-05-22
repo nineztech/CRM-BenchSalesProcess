77315ce191a6991ea124ef8dcc31826586217537
Dharmangi <129186750+Dharmangi@users.noreply.github.com>
2025-05-22 18:59:34 +0530

    Add files via upload

--- Admin/src/Components/Sidebar/Sidebar.tsx
+++ Admin/src/Components/Sidebar/Sidebar.tsx
@@ -2,49 +2,60 @@ import React, { useState } from 'react';
 import { FaTachometerAlt, FaUserPlus, FaUsers, FaBuilding } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './sidebar.css';
+import logoIcon from '../../assets/logo.webp'; // Make sure to update the path to your logo
 
 const Sidebar: React.FC = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    return (
-        <div 
-            className={`sidebar ${isExpanded ? 'expanded' : ''}`}
-            onMouseEnter={() => setIsExpanded(true)}
-            onMouseLeave={() => setIsExpanded(false)}
-        >
-            <ul>
-                <li>
-                  <FaTachometerAlt />
-                    {isExpanded && 
-                  <Link to="/dashboard">
-                    <span>Dashboard</span>
-                  </Link>}
-                    
-                </li>
-                <li>
-                    <FaUserPlus />
-                    {isExpanded && 
-                  <Link to="/adminregister">
-                    <span>Add Admin</span>
-                  </Link>}
-                </li>
-                <li>
-                    <FaUsers />
-                    {isExpanded && 
-                  <Link to="/adduser">
-                    <span>User Creation</span>
-                  </Link>}
-                </li>
-                <li>
-                    <FaBuilding />
-                    {isExpanded && 
-                  <Link to="/adddepartment">
-                    <span>Department</span>
-                  </Link>}
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div
+      className={`sidebar ${isExpanded ? 'expanded' : ''}`}
+      onMouseEnter={() => setIsExpanded(true)}
+      onMouseLeave={() => setIsExpanded(false)}
+    >
+      {/* Logo Section */}
+      <div className="sidebar-logo">
+        <img src={logoIcon} alt="Logo" className="logo-icon" />
+        {isExpanded && <span className="logo-text">NinezTech</span>}
+      </div>
+
+      {/* Menu List */}
+      <ul>
+        <li>
+          <FaTachometerAlt />
+          {isExpanded && (
+            <Link to="/dashboard">
+              <span>Dashboard</span>
+            </Link>
+          )}
+        </li>
+        <li>
+          <FaUserPlus />
+          {isExpanded && (
+            <Link to="/adminregister">
+              <span>Add Admin</span>
+            </Link>
+          )}
+        </li>
+        <li>
+          <FaUsers />
+          {isExpanded && (
+            <Link to="/adduser">
+              <span>User Creation</span>
+            </Link>
+          )}
+        </li>
+        <li>
+          <FaBuilding />
+          {isExpanded && (
+            <Link to="/adddepartment">
+              <span>Department</span>
+            </Link>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
