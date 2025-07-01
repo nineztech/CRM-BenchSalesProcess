6f8505db74b75087ba5ceb45c4c0d1ab42774037
hencytilwani <155488046+hencytilwani@users.noreply.github.com>
2025-07-01 05:16:03 -0400

    change

--- User/src/App.tsx
+++ User/src/App.tsx
@@ -1,12 +1,12 @@
 import { Toaster } from 'react-hot-toast';
-import AppRouter from './Routes/AppRouter';
+import Router from "./Routes/AppRouter"
 import './App.css';
 
 function App() {
   return (
     <>
       <Toaster position="top-right" />
-      <AppRouter />
+      <Router />
     </>
   );
 }
